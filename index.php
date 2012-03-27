f8b9713f183865e37b04927e9f25ab0219713517
KC <kc_reaney@mentor.com>
2012-03-27 15:35:53 -0700

    Updated index and router & cleaned up a bit

--- index.php
+++ index.php
@@ -1,99 +1,10 @@
 <?php
-
-
 error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 
-
-class Identity {
-
-	var $c = NULL;
-
-	var $connection = NULL; 
-	
-	function __construct(){
-		$this->connection = new Mongo();
-		$this->c = $this->connection->identity->users;
-
-	}
-
-	function search($query){
-		$q = explode(' ', $query);
-
-        $q0 = new MongoRegex("/^{$q[0]}/i");
-		if(count($q) > 1){
-            $q1 = new MongoRegex("/^{$q[1]}/i");
-
-			$search = array(
-				':or' => array(
-					array('fname' => $q0),
-					array('lname' => $q1),
-					//array('username' => $q0),
-					//rray('username' => $q1),
-				)
-			);
-		} else {
-			$search = array(
-				':or' => array(
-					array('fname' => $q0),
-					array('lname' => $q0),
-					array('username' => $q0),
-				),
-			);  
-		}
-		$tmp = $this->c->find($search);
-
-		//echo '<pre>';
-		header("Content-type: application/json");
-		echo json_encode(iterator_to_array($tmp));
-		//echo '</pre>';
-	}
-
-	function getByUsername($username){
-		header("Content-type: application/json");
-		echo json_encode($this->c->findOne(array('username' => $username)));
-	}
-	
-	function getPhotoByUsername($username) {
-		header("Content-type: image/jpeg");
-   		$img = file_get_contents("user-photos/$username.jpg", FILE_BINARY);
-		header("Content-Length: ".strlen($img));
-		echo $img;
-	}
-
-}
-
-function router(){
-	$ident = new Identity();
-
-	if(isset($_GET['q'])){
-		$request_path = strtok($_SERVER['REQUEST_URI'], '?');
-   	 	$base_path_len = strlen(rtrim(dirname($_SERVER['SCRIPT_NAME']), '\/'));
-    		// Unescape and strip $base_path prefix, leaving q without a leading slash.
-    		$path = substr(urldecode($request_path), $base_path_len + 1);
-
-		$parsed = filter_var_array(explode('/', trim($path, '/')), FILTER_SANITIZE_STRING);
-		if(count($parsed) > 1 ){
-			switch($parsed[0]) {
-				case 'search':
-					$ident->search($parsed[1]);
-				break;
-				
-				case 'users':
-					if($parsed[1] == 'by_username' && count($parsed) == 3){
-						$ident->getByUsername($parsed[2]);
-					}
-				break;
-				case 'photo':
-					if($parsed[1]){
-						$ident->getPhotoByUsername($parsed[1]);
-					}
-				break;
-			}
-		}
-	}
-}
-router();
+require_once 'lib/Router.php';
+$Router = new Router();
+$Router->route();
 
 ?>
--- lib/Router.php
+++ lib/Router.php
@@ -1,38 +1,55 @@
 <?php
-require_once './APIFactory.php';
+require_once './lib/APIFactory.php';
+include_once './lib/Exceptions.php';
 
 class Router
 {
-  function __construct(){
-  
-  }
 
-  function route($route){
+  public function route(){
     try {
-      $p = $this->parseUrl($route);
-      $obj = APIFactory::init($parsed);
-      switch(count($url_components)) { 
-      case 0: $obj->{$p[1]}(); break;
-      case 1: $obj->{$p[1]}($p[2]); break;
-      case 2: $obj->{$p[1]}($p[2], $p[3]); break;
-      case 3: $obj->{$p[1]}($p[2], $p[3], $p[4]); break;
-      case 4: $obj->{$p[1]}($p[2], $p[3], $p[4], $p[5]); break;
-      case 5: $obj->{$p[1]}($p[2], $p[3], $p[4], $p[5], $p[6]); break;
-      default: call_user_func_array(array($obj, $p[1]), array_splice($p, -2));  break;
-    } catch {
-      //@todo a nice graceful fail message
+      $p = $this->parsePath();
+      $obj = APIFactory::init($p[0]);
+
+      if($obj && count($p) > 1){
+        echo $this->getAction($obj, $p);
+      } else {
+        echo (!$obj) ? $this->handleNotFound() : $this->handleMissingAction();
+      }
+    } catch(UndefinedActionException $e) { 
+      echo $e->getMessage();
+    } catch(Exception $e) {
+      echo $e->getMessage();
     }
   }
 
-  function parseUrl($url){
+  public function parsePath(){
     $parsed = array();
-    if($url){
-      $request_path = strtok($_SERVER['REQUEST_URI'], '?');
-      $base_path_len = strlen(rtrim(dirname($_SERVER['SCRIPT_NAME']), '\/'));
-      // Unescape and strip $base_path prefix, leaving q without a leading slash.
-      $path = substr(urldecode($request_path), $base_path_len + 1);
-      $parsed = filter_var_array(explode('/', trim($path, '/')), FILTER_SANITIZE_STRING);
-    }
+    $request_path = strtok($_SERVER['REQUEST_URI'], '?');
+    $base_path_len = strlen(rtrim(dirname($_SERVER['SCRIPT_NAME']), '\/'));
+    // Unescape and strip $base_path prefix, leaving q without a leading slash.
+    $path = substr(urldecode($request_path), $base_path_len + 1);
+    $parsed = filter_var_array(explode('/', trim($path, '/')), FILTER_SANITIZE_STRING);
     return $parsed;
   }
+
+  private function getAction($obj, $p) {
+    if(!is_callable(array($obj, $p[1]))){
+      throw new UndefinedActionException('Action Undefined');
+    }
+    switch(count($p)) {
+      case 2: $obj->{$p[1]}(); break;
+      case 3: $obj->{$p[1]}($p[2]); break;
+      case 4: $obj->{$p[1]}($p[2], $p[3]); break;
+      case 5: $obj->{$p[1]}($p[2], $p[3], $p[4]); break;
+      case 6: $obj->{$p[1]}($p[2], $p[3], $p[4], $p[5]); break;
+      case 7: $obj->{$p[1]}($p[2], $p[3], $p[4], $p[5], $p[6]); break;
+      default: call_user_func_array(array($obj, $p[1]), array_splice($p, -2));  break;
+    }
+  }
+
+  private function handleNotFound() {
+    header("HTTP/1.0 404 Not Found");
+    return '404 Not Found';
+  }
+
 }
